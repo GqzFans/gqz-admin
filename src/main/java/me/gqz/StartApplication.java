02c86bf16f4d38f55159f1ba43a17c4338f1e5de
dragon <i@dragon-yuan.me>
2018-07-19 10:14:21 +0800

    [+]更改文件上传限制，最大20M

--- src/main/java/me/gqz/StartApplication.java
+++ src/main/java/me/gqz/StartApplication.java
@@ -2,6 +2,10 @@ package me.gqz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 
 /**
@@ -13,5 +17,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StartApplication {
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement(){
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // 设置文件大小限制,超了页面会抛出异常信息,这个时候需要进行异常处理
+        factory.setMaxFileSize(20 * 1024 * 1024);
+        // 设置总上传数据总大小
+        factory.setMaxRequestSize(20 * 1024 * 1024);
+        return factory.createMultipartConfig();
     }
 }
